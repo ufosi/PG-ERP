928680ebc7db3c6df0af653c8e661a0c9d63b288
ufosi <mark19951@gmail.com>
2026-05-25 20:43:32 +0200

    Add trustHost to NextAuth for IP address support

--- src/auth.ts
+++ src/auth.ts
@@ -15,6 +15,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     strategy: "jwt",
   },
   secret: process.env.AUTH_SECRET,
+  trustHost: true,
   pages: {
     signIn: "/login",
   },
